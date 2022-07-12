31d0382141774b97f9f9f114eba005a3941301a6
fjzheng <1059959730@qq.com>
2022-07-12 14:25:20 +0800

    add: :sparkles: 添加分页参数和结果类

---
+++ plasticene-common/src/main/java/com/plasticene/boot/common/utils/pojo/PageParam.java
@@ -0,0 +1,29 @@
+package com.plasticene.boot.common.utils.pojo;
+
+import lombok.Data;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/7/12 11:44
+ */
+@Data
+public class PageParam {
+    private static final Integer PAGE_NO = 1;
+    private static final Integer PAGE_SIZE = 20;
+
+//    @ApiModelProperty(value = "页码，从 1 开始", required = true,example = "1")
+    @NotNull(message = "页码不能为空")
+    @Min(value = 1, message = "页码最小值为 1")
+    private Integer pageNo = PAGE_NO;
+
+//    @ApiModelProperty(value = "每页条数，最大值为 100", required = true, example = "10")
+    @NotNull(message = "每页条数不能为空")
+    @Min(value = 1, message = "页码最小值为 1")
+    @Max(value = 100, message = "页码最大值为 200")
+    private Integer pageSize = PAGE_SIZE;
+}
---
+++ plasticene-common/src/main/java/com/plasticene/boot/common/utils/pojo/PageResult.java
@@ -0,0 +1,45 @@
+package com.plasticene.boot.common.utils.pojo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/7/12 11:43
+ */
+public final class PageResult<T> implements Serializable {
+
+//    @ApiModelProperty(value = "数据", required = true)
+    private List<T> list;
+
+//    @ApiModelProperty(value = "总量", required = true)
+    private Long total;
+
+    private Long pages;
+
+
+
+    public PageResult() {
+    }
+
+    public PageResult(List<T> list, Long total, Long pages) {
+        this.list = list;
+        this.total = total;
+        this.pages = pages;
+    }
+
+    public PageResult(Long total, Long pages) {
+        this.list = new ArrayList<>();
+        this.total = total;
+        this.pages = pages;
+    }
+
+    public static <T> PageResult<T> empty() {
+        return new PageResult<>(0L, 0L);
+    }
+
+
+}
+
