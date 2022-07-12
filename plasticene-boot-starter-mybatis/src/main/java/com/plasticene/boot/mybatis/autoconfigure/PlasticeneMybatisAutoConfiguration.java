01ea4f78945e13ea76ce26b49e8037dc5185acea
fjzheng <1059959730@qq.com>
2022-07-12 14:12:53 +0800

    update: :sparkles: 自动配置类

---
+++ plasticene-boot-starter-mybatis/src/main/java/com/plasticene/boot/mybatis/autoconfigure/PlasticeneMybatisAutoConfiguration.java
@@ -0,0 +1,29 @@
+package com.plasticene.boot.mybatis.autoconfigure;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.plasticene.boot.mybatis.core.handlers.DefaultDBFieldHandler;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/7/12 13:55
+ */
+@Configuration
+public class PlasticeneMybatisAutoConfiguration {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor()); // 分页插件
+        return mybatisPlusInterceptor;
+    }
+
+    @Bean
+    public MetaObjectHandler defaultMetaObjectHandler(){
+        return new DefaultDBFieldHandler();
+    }
+}
