b6221bf709498cf24b2976a243b352e18dc39978
fjzheng <1059959730@qq.com>
2022-07-14 15:58:39 +0800

    add: :sparkles: 添加拦截器，该拦截器执行顺序优先级最高

---
+++ plasticene-boot-starter-web/src/main/java/com/plasticene/boot/web/core/filter/WebTraceFilter.java
@@ -0,0 +1,44 @@
+package com.plasticene.boot.web.core.filter;
+
+import cn.hutool.core.util.StrUtil;
+import com.plasticene.boot.web.core.prop.TraceProperties;
+import com.plasticene.boot.web.core.utils.MDCTraceUtils;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.annotation.Resource;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/7/13 18:20
+ */
+//
+public class WebTraceFilter extends OncePerRequestFilter {
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws IOException, ServletException {
+        try {
+            String traceId = request.getHeader(MDCTraceUtils.TRACE_ID_HEADER);
+            String spanId = request.getHeader(MDCTraceUtils.SPAN_ID_HEADER);
+            if (StrUtil.isEmpty(traceId)) {
+                MDCTraceUtils.addTrace();
+            } else {
+                MDCTraceUtils.putTrace(traceId, spanId);
+            }
+            filterChain.doFilter(request, response);
+        } finally {
+            MDCTraceUtils.removeTrace();
+        }
+    }
+}
