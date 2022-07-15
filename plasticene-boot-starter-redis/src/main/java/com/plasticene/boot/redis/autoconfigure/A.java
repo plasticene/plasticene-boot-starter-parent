6597297019726cb358019d6679b83eca1ca10f28
fjzheng <1059959730@qq.com>
2022-07-15 11:02:42 +0800

    add: :sparkles: 添加redis封装模块

---
+++ plasticene-boot-starter-redis/src/main/java/com/plasticene/boot/redis/autoconfigure/A.java
@@ -0,0 +1,9 @@
+package com.plasticene.boot.redis.autoconfigure;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/7/15 10:47
+ */
+public class A {
+}
---
+++ plasticene-boot-starter-redis/src/main/java/com/plasticene/boot/redis/core/B.java
@@ -0,0 +1,9 @@
+package com.plasticene.boot.redis.core;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/7/15 11:02
+ */
+public class B {
+}
