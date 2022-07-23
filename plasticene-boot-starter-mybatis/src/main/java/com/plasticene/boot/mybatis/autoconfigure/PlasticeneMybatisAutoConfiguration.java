44149d62681702a540c5d4d46d1eb4847cce48e4
fjzheng <1059959730@qq.com>
2022-07-23 10:08:15 +0800

    add: :sparkles: 加密bean注入

--- plasticene-boot-starter-mybatis/src/main/java/com/plasticene/boot/mybatis/autoconfigure/PlasticeneMybatisAutoConfiguration.java
+++ plasticene-boot-starter-mybatis/src/main/java/com/plasticene/boot/mybatis/autoconfigure/PlasticeneMybatisAutoConfiguration.java
@@ -3,9 +3,19 @@ package com.plasticene.boot.mybatis.autoconfigure;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.plasticene.boot.mybatis.core.encrypt.AESEncryptService;
+import com.plasticene.boot.mybatis.core.encrypt.Base64EncryptService;
+import com.plasticene.boot.mybatis.core.encrypt.EncryptService;
+import com.plasticene.boot.mybatis.core.enums.Algorithm;
 import com.plasticene.boot.mybatis.core.handlers.DefaultDBFieldHandler;
+import com.plasticene.boot.mybatis.core.handlers.EncryptTypeHandler;
+import com.plasticene.boot.mybatis.core.prop.EncryptProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
 
 /**
  * @author fjzheng
@@ -13,7 +23,10 @@ import org.springframework.context.annotation.Configuration;
  * @date 2022/7/12 13:55
  */
 @Configuration
+@EnableConfigurationProperties(EncryptProperties.class)
 public class PlasticeneMybatisAutoConfiguration {
+    @Resource
+    private EncryptProperties encryptProperties;
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
@@ -24,5 +37,28 @@ public class PlasticeneMybatisAutoConfiguration {
     @Bean
     public MetaObjectHandler defaultMetaObjectHandler(){
         return new DefaultDBFieldHandler();
+    }
+
+    @Bean
+    public EncryptTypeHandler encryptTypeHandler() {
+        return new EncryptTypeHandler();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(EncryptService.class)
+    public EncryptService encryptService() {
+        Algorithm algorithm = encryptProperties.getAlgorithm();
+        EncryptService encryptService;
+        switch (algorithm) {
+            case BASE64:
+                encryptService =  new Base64EncryptService();
+                break;
+            case AES:
+                encryptService = new AESEncryptService();
+                break;
+            default:
+                encryptService =  null;
+        }
+        return encryptService;
     }
 }
