1f28ec6aae9562a54a510b59a45ca93b7f0cee51
fjzheng <1059959730@qq.com>
2022-07-27 09:59:21 +0800

    add: :sparkles: 添加feign拦截器实现服务调用间调用请求header和上下文traceId的传递

---
+++ plasticene-boot-starter-web/src/main/java/com/plasticene/boot/web/interceptor/FeignInterceptor.java
@@ -0,0 +1,46 @@
+package com.plasticene.boot.web.interceptor;
+
+import cn.hutool.core.util.StrUtil;
+import com.plasticene.boot.web.core.utils.MDCTraceUtils;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+import java.util.Objects;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/7/26 17:09
+ */
+public class FeignInterceptor implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+         // 传递header
+        if (requestAttributes != null) {
+            HttpServletRequest request = requestAttributes.getRequest();
+            Enumeration<String> headerNames = request.getHeaderNames();
+            if (headerNames != null) {
+                while (headerNames.hasMoreElements()) {
+                    String name = headerNames.nextElement();
+                    // 跳过 content-length
+                    if (Objects.equals("content-length", name)){
+                        continue;
+                    }
+                    String value = request.getHeader(name);
+                    requestTemplate.header(name, value);
+                }
+            }
+        }
+        // 传递日志traceId
+        String traceId = MDCTraceUtils.getTraceId();
+        if (StrUtil.isNotEmpty(traceId)) {
+            requestTemplate.header(MDCTraceUtils.TRACE_ID_HEADER, traceId);
+            requestTemplate.header(MDCTraceUtils.SPAN_ID_HEADER, MDCTraceUtils.getNextSpanId());
+        }
+    }
+}
