2ff6c5d6d06bcdc7f5dacc93ab7704753c9ffad0
fjzheng <1059959730@qq.com>
2022-07-27 11:19:27 +0800

    update: :art: 提供分页参数类构造方法

--- plasticene-common/src/main/java/com/plasticene/boot/common/pojo/PageParam.java
+++ plasticene-common/src/main/java/com/plasticene/boot/common/pojo/PageParam.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 /**
  * @author fjzheng
@@ -16,14 +17,25 @@ public class PageParam {
     private static final Integer PAGE_NO = 1;
     private static final Integer PAGE_SIZE = 20;
 
-//    @ApiModelProperty(value = "页码，从 1 开始", required = true,example = "1")
     @NotNull(message = "页码不能为空")
     @Min(value = 1, message = "页码最小值为 1")
     private Integer pageNo = PAGE_NO;
 
-//    @ApiModelProperty(value = "每页条数，最大值为 100", required = true, example = "10")
     @NotNull(message = "每页条数不能为空")
     @Min(value = 1, message = "页码最小值为 1")
-    @Max(value = 100, message = "页码最大值为 200")
+    @Max(value = 200, message = "页码最大值为 200")
     private Integer pageSize = PAGE_SIZE;
+
+    public PageParam() {
+
+    }
+
+    public PageParam(Integer pageNo, Integer pageSize) {
+        if (Objects.nonNull(pageNo)) {
+            this.pageNo = pageNo;
+        }
+       if (Objects.nonNull(pageSize)) {
+           this.pageSize = pageSize;
+       }
+    }
 }
