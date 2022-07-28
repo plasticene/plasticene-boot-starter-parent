773863dfaa464e8ee9aa53b084b70a08191a5d19
fjzheng <1059959730@qq.com>
2022-07-28 10:36:21 +0800

    add: :sparkles: 添加web默认配置jackson和mvc

--- plasticene-boot-starter-web/src/main/java/com/plasticene/boot/web/autoconfigure/PlasticeneWebAutoConfiguration.java
+++ plasticene-boot-starter-web/src/main/java/com/plasticene/boot/web/autoconfigure/PlasticeneWebAutoConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.concurrent.ExecutorService;
 
@@ -24,6 +25,7 @@ import java.util.concurrent.ExecutorService;
  */
 @Configuration
 @EnableConfigurationProperties({TraceProperties.class, ThreadPoolProperties.class})
+@PropertySource("classpath:/web-default.properties")
 public class PlasticeneWebAutoConfiguration {
 
 
