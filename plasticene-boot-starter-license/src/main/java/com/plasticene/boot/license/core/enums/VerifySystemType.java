c6c64631ff37176c28cdef5d884b8ff396776734
fjzheng <1059959730@qq.com>
2022-08-02 16:16:56 +0800

    add: :sparkles: 添加license校验系统信息方式

---
+++ plasticene-boot-starter-license/src/main/java/com/plasticene/boot/license/core/enums/VerifySystemType.java
@@ -0,0 +1,11 @@
+package com.plasticene.boot.license.core.enums;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/8/2 10:33
+ */
+public enum VerifySystemType {
+    SYSTEM_UUID,
+    CPU_ID;
+}
