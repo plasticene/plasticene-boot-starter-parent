845fe97ee8d34a4237ab4edc19e60f89804d7cf9
fjzheng <1059959730@qq.com>
2022-08-02 16:17:50 +0800

    update: :art: license验证逻辑添加验证系统信息

--- plasticene-boot-starter-license/src/main/java/com/plasticene/boot/license/core/LicenseVerify.java
+++ plasticene-boot-starter-license/src/main/java/com/plasticene/boot/license/core/LicenseVerify.java
@@ -1,8 +1,11 @@
 package com.plasticene.boot.license.core;
 
 import com.plasticene.boot.common.exception.BizException;
+import com.plasticene.boot.license.core.enums.VerifySystemType;
 import com.plasticene.boot.license.core.param.CustomKeyStoreParam;
+import com.plasticene.boot.license.core.param.SystemInfo;
 import com.plasticene.boot.license.core.prop.LicenseProperties;
+import com.plasticene.boot.license.core.utils.DmcUtils;
 import de.schlichtherle.license.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,6 +15,7 @@ import javax.annotation.Resource;
 import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Objects;
 import java.util.prefs.Preferences;
 
 /**
@@ -53,6 +57,21 @@ public class LicenseVerify {
         try {
             LicenseManager licenseManager = new LicenseManager(initLicenseParam());
             LicenseContent licenseContent = licenseManager.verify();
+            if (licenseProperties.getVerifySystemSwitch()) {
+                SystemInfo systemInfo = (SystemInfo) licenseContent.getExtra();
+                VerifySystemType verifySystemType = licenseProperties.getVerifySystemType();
+                switch (verifySystemType) {
+                    case CPU_ID:
+                        checkCpuId(systemInfo.getCpuId());
+                        break;
+                    case SYSTEM_UUID:
+                        checkSystemUuid(systemInfo.getUuid());
+                        break;
+                    default:
+                        checkCpuId(systemInfo.getCpuId());
+                }
+
+            }
             logger.info("证书校验通过，证书有效期：{} - {}",df.format(licenseContent.getNotBefore()),
                     df.format(licenseContent.getNotAfter()));
             return true;
@@ -80,6 +99,24 @@ public class LicenseVerify {
                 ,preferences
                 ,publicStoreParam
                 ,cipherParam);
+    }
+
+    private void checkCpuId(String cpuId) {
+        cpuId = cpuId.trim().toUpperCase();
+        String systemCpuId = DmcUtils.getCpuId().trim().toUpperCase();
+        logger.info("配置cpuId = {},  系统cpuId = {}", cpuId, systemCpuId);
+        if (!Objects.equals(cpuId, systemCpuId)) {
+            throw new BizException("license检验cpuId不一致");
+        }
+    }
+
+    private void checkSystemUuid(String uuid) {
+        uuid = uuid.trim().toUpperCase();
+        String systemUuid = DmcUtils.getSystemUuid().trim().toUpperCase();
+        logger.info("配置uuid = {},  系统uuid= {}", uuid, systemUuid);
+        if (!Objects.equals(uuid, systemUuid)) {
+            throw new BizException("license检验uuid不一致");
+        }
     }
 
 }
