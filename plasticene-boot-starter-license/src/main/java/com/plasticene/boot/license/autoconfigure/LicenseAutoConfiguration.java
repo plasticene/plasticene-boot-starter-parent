e89b151d431636eb4992d4b6aa8ca95460788fda
fjzheng <1059959730@qq.com>
2022-08-03 15:42:39 +0800

    update: :art: 自动配置注入相关组件

--- plasticene-boot-starter-license/src/main/java/com/plasticene/boot/license/autoconfigure/LicenseAutoConfiguration.java
+++ plasticene-boot-starter-license/src/main/java/com/plasticene/boot/license/autoconfigure/LicenseAutoConfiguration.java
@@ -1,7 +1,11 @@
 package com.plasticene.boot.license.autoconfigure;
 
+import com.plasticene.boot.license.core.LicenseCheckApplicationRunner;
+import com.plasticene.boot.license.core.aop.LicenseAspect;
 import com.plasticene.boot.license.core.prop.LicenseProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,4 +18,20 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = {"com.plasticene.boot.license"})
 @EnableConfigurationProperties(LicenseProperties.class)
 public class LicenseAutoConfiguration {
+
+
+    @Bean
+    @ConditionalOnProperty(name = "ptc.license.start-check", havingValue = "true", matchIfMissing = true)
+    public LicenseCheckApplicationRunner licenseCheckApplicationRunner() {
+        return new LicenseCheckApplicationRunner();
+    }
+
+    @Bean
+    public LicenseAspect licenseAspect() {
+        return new LicenseAspect();
+    }
+
+
+
+
 }
