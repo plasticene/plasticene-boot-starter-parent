95aaadcb24bb7abe054ba82a5a7f94fa62fc7d11
fjzheng <1059959730@qq.com>
2023-04-21 11:12:07 +0800

    add: :rocket: 添加oss starter依赖

---
+++ plasticene-boot-starter-oss/src/main/java/com/plasticene/boot/oss/autoconfigure/OssAutoConfiguration.java
@@ -0,0 +1,56 @@
+package com.plasticene.boot.oss.autoconfigure;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.plasticene.boot.oss.core.OssClient;
+import com.plasticene.boot.oss.core.OssTemplate;
+import com.plasticene.boot.oss.core.prop.OssProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2023/4/20 13:56
+ */
+@Configuration
+@EnableConfigurationProperties(OssProperties.class)
+public class OssAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(AmazonS3.class)
+    public AmazonS3 ossClient(OssProperties ossProperties) {
+        // 客户端配置，主要是全局的配置信息
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.setMaxConnections(ossProperties.getMaxConnections());
+        // url以及region配置
+        AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(
+                ossProperties.getEndpoint(), ossProperties.getRegion());
+        // 凭证配置
+        AWSCredentials awsCredentials = new BasicAWSCredentials(ossProperties.getAccessKey(),
+                ossProperties.getSecretKey());
+        AWSCredentialsProvider awsCredentialsProvider = new AWSStaticCredentialsProvider(awsCredentials);
+        // build amazonS3Client客户端
+        return AmazonS3Client.builder().withEndpointConfiguration(endpointConfiguration)
+                .withClientConfiguration(clientConfiguration).withCredentials(awsCredentialsProvider)
+                .disableChunkedEncoding().withPathStyleAccessEnabled(ossProperties.getPathStyleAccess()).build();
+    }
+
+    @Bean
+    @ConditionalOnBean(AmazonS3.class)
+    @ConditionalOnMissingBean(OssTemplate.class)
+    public OssTemplate ossTemplate(AmazonS3 amazonS3){
+        return new OssClient(amazonS3);
+    }
+
+
+}
