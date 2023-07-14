6960d28660d80e05bada897bea32ab10f65e4f31
fjzheng <1059959730@qq.com>
2023-07-14 10:19:48 +0800

    add: :rocket: 优化全局异常统一处理判断

--- plasticene-boot-starter-web/src/main/java/com/plasticene/boot/web/core/global/GlobalExceptionHandler.java
+++ plasticene-boot-starter-web/src/main/java/com/plasticene/boot/web/core/global/GlobalExceptionHandler.java
@@ -6,11 +6,15 @@ import com.plasticene.boot.common.pojo.ResponseVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -41,6 +45,7 @@ public class GlobalExceptionHandler {
             }
             return ResponseVO.failure(bizException.getCode(), bizException.getMessage());
         } else if (e instanceof MethodArgumentNotValidException) {
+            // 参数检验异常
             MethodArgumentNotValidException methodArgumentNotValidException = (MethodArgumentNotValidException) e;
             Map<String, String> map = new HashMap<>();
             BindingResult result = methodArgumentNotValidException.getBindingResult();
@@ -49,8 +54,23 @@ public class GlobalExceptionHandler {
                 String field = item.getField();
                 map.put(field, message);
             });
-            log.error("数据校验出现问题:{},异常类型{}", methodArgumentNotValidException.getMessage(), methodArgumentNotValidException.getClass());
+            log.error("数据校验出现错误：", e);
             return ResponseVO.failure(ResponseStatusEnum.BAD_REQUEST, map);
+        } else if (e instanceof HttpRequestMethodNotSupportedException) {
+            log.error("请求方法错误：", e);
+            return ResponseVO.failure(ResponseStatusEnum.BAD_REQUEST.getCode(), "请求方法不正确");
+        } else if (e instanceof MissingServletRequestParameterException) {
+            log.error("请求参数缺失：", e);
+            MissingServletRequestParameterException ex = (MissingServletRequestParameterException) e;
+            return ResponseVO.failure(ResponseStatusEnum.BAD_REQUEST.getCode(), "请求参数缺少: " + ex.getParameterName());
+        } else if (e instanceof MethodArgumentTypeMismatchException) {
+            log.error("请求参数类型错误：", e);
+            MethodArgumentTypeMismatchException ex = (MethodArgumentTypeMismatchException) e;
+            return ResponseVO.failure(ResponseStatusEnum.BAD_REQUEST.getCode(), "请求参数类型不正确：" + ex.getName());
+        } else if (e instanceof NoHandlerFoundException) {
+            NoHandlerFoundException ex = (NoHandlerFoundException) e;
+            log.error("请求地址不存在：", e);
+            return ResponseVO.failure(ResponseStatusEnum.NOT_EXIST, ex.getRequestURL());
         } else {
             //如果是系统的异常，比如空指针这些异常
             log.error("【系统异常】", e);
