9a5930e87d3fd11886915ce203345cb6ab8cf2e1
fjzheng <1059959730@qq.com>
2025-05-19 15:01:32 +0800

    add: :rocket: 添加banner使用示例

---
+++ plasticene-boot-example/plasticene-boot-banner-example/src/main/java/com/plasticene/boot/example/banner/BannerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.plasticene.boot.example.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BannerExampleApplication.class, args);
+    }
+
+}
---
+++ plasticene-boot-example/plasticene-boot-banner-example/src/test/java/com/plasticene/boot/example/banner/PlasticeneBootBannerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plasticene.boot.example.banner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlasticeneBootBannerExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
