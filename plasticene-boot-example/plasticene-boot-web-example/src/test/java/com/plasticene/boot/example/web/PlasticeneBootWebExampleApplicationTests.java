ef85b764f8918276749ccfd9d91145f265df5adb
fjzheng <1059959730@qq.com>
2025-05-21 15:21:19 +0800

    add: :tada: 初步提交web测试示例模块

---
+++ plasticene-boot-example/plasticene-boot-web-example/src/test/java/com/plasticene/boot/example/web/PlasticeneBootWebExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plasticene.boot.example.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlasticeneBootWebExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
