afafc27f8c98f20d88b913efa3304f40e3454393
fjzheng <1059959730@qq.com>
2025-05-23 11:27:29 +0800

    update: :white_check_mark: 测试通过接口参数加解密

--- plasticene-boot-example/plasticene-boot-web-example/src/main/java/com/plasticene/boot/example/web/controller/TestController.java
+++ plasticene-boot-example/plasticene-boot-web-example/src/main/java/com/plasticene/boot/example/web/controller/TestController.java
@@ -1,14 +1,31 @@
 package com.plasticene.boot.example.web.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.plasticene.boot.common.pojo.ResponseVO;
 import com.plasticene.boot.example.web.param.UserParam;
 import com.plasticene.boot.example.web.vo.UserVO;
 import com.plasticene.boot.web.core.anno.ApiLog;
+import com.plasticene.boot.web.core.anno.ApiSecurity;
 import com.plasticene.boot.web.core.anno.ResponseResultBody;
+import com.plasticene.boot.web.core.prop.ApiSecurityProperties;
+import com.plasticene.boot.web.core.utils.AESUtil;
+import com.plasticene.boot.web.core.utils.RSAUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.weaver.ast.Var;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.List;
 
 /**
  * @author fjzheng
@@ -22,6 +39,8 @@ import org.springframework.web.bind.annotation.*;
 @ApiLog
 @ResponseResultBody
 public class TestController {
+    @Resource
+    private ApiSecurityProperties apiSecurityProperties;
 
     @Operation(summary = "获取消息")
     @GetMapping("/msg")
@@ -46,4 +65,56 @@ public class TestController {
     public UserVO getUser() {
         return UserVO.builder().userId(10L).userName("plasticene").build();
     }
+
+    @Operation(summary = "加密出入参数接口，供调用测试(加解密接口必须是post)")
+    @PostMapping("/user/list")
+    @ApiSecurity(isSign = false, decryptRequest = true, encryptResponse = true)
+    public ResponseVO<List<UserVO>> listUsers(@RequestBody UserParam param) {
+        String name = StringUtils.isNotBlank(param.getName()) ? param.getName() : "张三";
+        Integer gender = param.getGender();
+        List<UserVO> voList = List.of(
+                UserVO.builder().userId(10L).userName(name).age(gender == 1 ? 18 : 20).build(),
+                UserVO.builder().userId(20L).userName(name + "20").age(gender == 0 ? 28 : 30).build()
+        );
+        log.info("voList===={}", voList);
+        return ResponseVO.success(voList);
+    }
+
+    @Operation(summary = "测试调加密接口")
+    @GetMapping("/api/security")
+    public ResponseVO<List<UserVO>> testSecurityApi() throws IOException, InterruptedException {
+        UserParam param = new UserParam();
+        param.setName("哈哈");
+        param.setGender(1);
+
+        String aesKey = AESUtil.generateAESKey();
+        String data = AESUtil.encrypt(JSON.toJSONString(param), aesKey);
+        String key = RSAUtil.encryptByPublicKey(aesKey, apiSecurityProperties.getRsaPublicKey());
+        JSONObject req = new JSONObject();
+        req.put("key", key);
+        req.put("data", data);
+        HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(req.toJSONString());
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://127.0.0.1:8882/test/user/list"))
+                .header("Content-Type", "application/json")
+                .POST(body)
+                .timeout(Duration.ofSeconds(5))
+                .build();
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(10))
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        HttpResponse<String> httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        ResponseVO<?> responseVO = JSON.parseObject(httpResponse.body(), ResponseVO.class);
+        Object rst = responseVO.getData();
+        JSONObject res = JSON.parseObject(rst.toString());
+        String decryptKey = RSAUtil.decryptByPrivateKey((String) res.get("key"), apiSecurityProperties.getRsaPrivateKey());
+        String resData = AESUtil.decrypt((String) res.get("data"), decryptKey);
+        log.info("resData===={}", resData);
+        return ResponseVO.success(JSON.parseArray(resData, UserVO.class));
+
+
+    }
+
 }
