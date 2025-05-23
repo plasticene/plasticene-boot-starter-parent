23804ff88a06aa81a692371ab18a9836ef53c6bb
fjzheng <1059959730@qq.com>
2025-05-23 15:45:17 +0800

    test :white_check_mark: 测试接口参数加解密动态密钥

--- plasticene-boot-example/plasticene-boot-web-example/src/main/java/com/plasticene/boot/example/web/WebExampleApplication.java
+++ plasticene-boot-example/plasticene-boot-web-example/src/main/java/com/plasticene/boot/example/web/WebExampleApplication.java
@@ -1,7 +1,10 @@
 package com.plasticene.boot.example.web;
 
+import com.plasticene.boot.example.web.provider.DatabaseApiKeySecurityProvider;
+import com.plasticene.boot.web.core.advice.ApiSecurityKeyProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author fjzheng
@@ -11,6 +14,11 @@ public class WebExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebExampleApplication.class, args);
+    }
+
+    @Bean
+    public ApiSecurityKeyProvider apiSecurityKeyProvider() {
+        return new DatabaseApiKeySecurityProvider();
     }
 
 }
