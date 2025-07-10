4fcb483754f62674345a0d0e9287408c44ad5007
fjzheng <1059959730@qq.com>
2025-07-10 14:21:43 +0800

    feat: :rocket: 删除代码

--- plasticene-boot-starter-mybatis/src/main/java/com/plasticene/boot/mybatis/core/mapper/BaseMapperX.java
+++ plasticene-boot-starter-mybatis/src/main/java/com/plasticene/boot/mybatis/core/mapper/BaseMapperX.java
@@ -79,18 +79,4 @@ public interface BaseMapperX<T> extends BaseMapper<T> {
         return selectList(new LambdaQueryWrapper<T>().in(field, values));
     }
 
-    /**
-     * 逐条插入，适合少量数据插入，或者对性能要求不高的场景
-     *
-     * 如果大量，请使用 {@link com.baomidou.mybatisplus.extension.service.impl.ServiceImpl#saveBatch(Collection)} 方法
-     *
-     * @param entities 实体们
-     */
-    default void insertBatch(Collection<T> entities) {
-        entities.forEach(this::insert);
-    }
-
-    default void updateBatch(T update) {
-        update(update, new QueryWrapper<>());
-    }
 }
