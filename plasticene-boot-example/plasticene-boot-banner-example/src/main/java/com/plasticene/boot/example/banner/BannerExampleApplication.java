c2c81a20807d99c38d61af78f9518010caa82de2
fjzheng <1059959730@qq.com>
2025-09-02 11:20:48 +0800

    chore: :arrow_up: jdk从17升级到21

--- plasticene-boot-example/plasticene-boot-banner-example/src/main/java/com/plasticene/boot/example/banner/BannerExampleApplication.java
+++ plasticene-boot-example/plasticene-boot-banner-example/src/main/java/com/plasticene/boot/example/banner/BannerExampleApplication.java
@@ -3,6 +3,10 @@ package com.plasticene.boot.example.banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ZFJ
+ * @date 2025/7/16
+ */
 @SpringBootApplication
 public class BannerExampleApplication {
 
