176a655177c57a10f2fcf73b4f1f0be0149b9f35
fjzheng <1059959730@qq.com>
2025-09-03 17:55:26 +0800

    feat: :tada: 添加flow测试用例模块

---
+++ plasticene-boot-example/plasticene-boot-flow-example/src/main/java/com/plasticene/boot/example/flow/FlowExampleApplication.java
@@ -0,0 +1,17 @@
+package com.plasticene.boot.example.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZFJ
+ */
+@SpringBootApplication
+public class FlowExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowExampleApplication.class, args);
+    }
+
+
+}
---
+++ plasticene-boot-example/plasticene-boot-flow-example/src/test/java/com/plasticene/boot/example/flow/PlasticeneBootFlowExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plasticene.boot.example.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlasticeneBootFlowExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
