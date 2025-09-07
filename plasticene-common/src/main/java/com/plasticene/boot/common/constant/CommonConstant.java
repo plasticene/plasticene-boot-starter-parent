03b87710992e26d926da35707a861d3007cb383d
fjzheng <1059959730@qq.com>
2025-09-08 00:12:11 +0800

    feat :sparkles: 添加全局公用常量

---
+++ plasticene-common/src/main/java/com/plasticene/boot/common/constant/CommonConstant.java
@@ -0,0 +1,30 @@
+package com.plasticene.boot.common.constant;
+
+/**
+ * @author ZFJ
+ * @date 2025/9/5
+ */
+public interface CommonConstant {
+
+    /**
+     * 开启  是  true
+     */
+
+    Integer IS_ON = 1;
+    /**
+     * 关闭 否  false
+     */
+    Integer IS_OFF = 0;
+
+    /**
+     * 已删除
+     */
+    Integer IS_DEL = 1;
+
+    /**
+     * 未删除
+     */
+    Integer IS_NOT_DEL = 0;
+
+
+}
