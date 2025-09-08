71c041c30336bb8b809bfe504c43f94a1bd7dc3c
fjzheng <1059959730@qq.com>
2025-09-08 15:14:24 +0800

    feat :sparkles: 测试实例事件监听器

---
+++ plasticene-boot-example/plasticene-boot-flow-example/src/main/java/com/plasticene/boot/example/flow/LeaveFlowInstanceListener.java
@@ -0,0 +1,24 @@
+package com.plasticene.boot.example.flow;
+
+import com.plasticene.boot.flow.core.event.BaseFlowInstanceListener;
+import com.plasticene.boot.flow.core.event.InstanceEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ZFJ
+ * @date 2025/9/8
+ */
+@Component
+@Slf4j
+public class LeaveFlowInstanceListener extends BaseFlowInstanceListener {
+    @Override
+    public void doProcessInstanceEvent(InstanceEvent event) {
+        log.info("======>>>instance event {}", event);
+    }
+
+    @Override
+    public String businessType() {
+        return "wf:leave";
+    }
+}
