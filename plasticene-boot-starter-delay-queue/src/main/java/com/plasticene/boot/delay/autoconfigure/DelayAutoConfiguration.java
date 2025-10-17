62676b7552c0639c94958aba3367b63195664fe8
fjzheng <1059959730@qq.com>
2025-10-17 16:01:13 +0800

    feat: :sparkles:  添加自动配置类

--- plasticene-boot-starter-delay-queue/src/main/java/com/plasticene/boot/delay/autoconfigure/DelayAutoConfiguration.java
+++ plasticene-boot-starter-delay-queue/src/main/java/com/plasticene/boot/delay/autoconfigure/DelayAutoConfiguration.java
@@ -1,7 +1,14 @@
 package com.plasticene.boot.delay.autoconfigure;
 
+import com.plasticene.boot.delay.core.DistributedDelayQueue;
+import com.plasticene.boot.delay.core.coordinator.Coordinator;
+import com.plasticene.boot.delay.core.coordinator.RedisCoordinator;
 import com.plasticene.boot.delay.core.prop.DelayProperties;
+import com.plasticene.boot.delay.core.storage.RedisTaskStorage;
+import com.plasticene.boot.delay.core.storage.TaskStorage;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,4 +18,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties({DelayProperties.class})
 public class DelayAutoConfiguration {
+
+    @ConditionalOnMissingBean(Coordinator.class)
+    @Bean
+    public Coordinator coordinator() {
+        return new RedisCoordinator();
+    }
+
+    @ConditionalOnMissingBean(TaskStorage.class)
+    @Bean
+    public TaskStorage taskStorage() {
+        return new RedisTaskStorage();
+    }
+
+    @Bean
+    public DistributedDelayQueue distributedDelayQueue() {
+        return new DistributedDelayQueue();
+    }
+
 }
