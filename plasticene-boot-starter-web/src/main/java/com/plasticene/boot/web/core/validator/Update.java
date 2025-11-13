bdbb46c3db4e071ea6dbecea1b9a57b85847601c
fjzheng <1059959730@qq.com>
2025-11-13 18:22:46 +0800

    feat: :sparkles: 参数校验分组

---
+++ plasticene-boot-starter-web/src/main/java/com/plasticene/boot/web/core/validator/Update.java
@@ -0,0 +1,10 @@
+package com.plasticene.boot.web.core.validator;
+
+import jakarta.validation.groups.Default;
+
+/**
+ * @author ZFJ
+ * @date 2025/11/13
+ */
+public interface Update extends Default {
+}
